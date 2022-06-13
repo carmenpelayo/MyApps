cf729defedd8e87d45b652970f664f99bedc6ec6
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-06-12 20:43:15 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -170,9 +170,9 @@ def page2():
     
     recommendation = recommendation(input_vector, weights_vector) 
     
-#     #Plotting results
-     best = recommendation.iloc[1:6]
-     st.write(best)
+    #Plotting results
+    best = recommendation.iloc[1:6]
+    st.write(best)
 #     dfcoord = pd.read_excel('nuts2.xlsx')
 #     bestcoord = pd.DataFrame()
 #     for i in range(len(best)):
