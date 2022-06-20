b9dfbc09b29b7942e76810fd653132850d4b9ee1
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-06-20 15:37:29 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -15,7 +15,10 @@ from scipy.spatial import distance
 def main_page():
     st.title("Location Recommendation System for Businesses in the European ICT Industry")
     st.subheader('This web was made by Carmen Pelayo Fernandez in 2022')
-    st.write("Given the high importance of making the right business location choices, this tool aims to provide a decision-making support solution for entities operating in the European ICT (Information and Communication Technologies) industry.")
+    st.write("""Given the high importance of making the right business location choices, this tool aims to provide a decision-making support solution 
+             for entities operating in the European ICT (Information and Communication Technologies) industry. It can also be configured to fit other purposes, 
+             like the search of specialized employment, the visualization of socio-economic data or the discovery of available capital funding.
+             """)
     image = Image.open('How-to-Find-the-Perfect-Location-for-Your-Small-Business.jpg')
     st.image(image)
     
