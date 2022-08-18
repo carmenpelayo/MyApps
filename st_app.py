aabeb09e2bec70f3624292db3c1e77d2c66a43db
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-08-19 01:50:37 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -259,17 +259,48 @@ def comparator():
     regions = dfn["NUTS 2 Code"].tolist()
     dfn = dfn.set_index("NUTS 2 Code")
     
+    #CONFIGURATOR
     st.header("REGION COMPARATOR")
-    st.write("Which regions would you like to compare?")
+    st.subheader("Which regions would you like to compare?")
     reg = st.multiselect("", regions, "ES30")
+    
+    #RESULTS
     st.table(dfn.loc[reg])
     
     #Results interpretation
-    st.subheader("How to read the scores obtained:")
-    st.write("Scores are given in a -1 to 1 scale.")
+    st.subheader("How to interpret the parameters displayed:")
     st.write(
         """
-        - A score of 0 represents the mean score of the regions.A score of -1 represents the region is in the lower 15.9% of the regions. Lower scores correspond to left outliers.
+        - AI: Total value of AI (Artificial Intelligence) projects developed in the region.
+        - Big Data: Total value of Big Data projects developed in the region.
+        - Computing: Total value of Computing projects developed in the region.
+        - Cybersecurity: Total value of Cybersecurityprojects developed in the region.
+        - Internet: Total value of Internet projects developed in the region.
+        - IoT: Total value of IoT projects developed in the region.
+        - Media & Communication: Total value of Media & Communication projects developed in the region.
+        - Robotics: Total value of Robotics projects developed in the region.
+        - Software: Total value of Software projects developed in the region.
+        - SMEs: Number of SMEs (Small and Mid-sized Enterprises) in a region (those with no more than 250 employees).
+        - LEs: Number of LEs (Large Enterprises) in a region (those with more than 250 employees).
+        - Research: Number of organizations focused on developing new technological offerings based on engineering innovation or scientific discoveries and advances (universities or other higher-level research institutions).
+        - Development: Number of organizations focused on R&D, to build and enhance existing technologies (technological centers or research organizations).
+        - Integration: Number entities dedicated to integrating existing technologies with products or services in order to expand business capabilities (firms).
+        - EU grants: Total euro amount that the European Union has given to local entities through the Horizon2020 program.
+        - Capital: Score calculated as the average of FDI (Foreign Direct Investments) & technology transfer, R&D expenditure (% of GDP) and venture capital availability.
+        - Unicorns: Total valuation of all the unicorns in a region. Unicorns are start-up companies valued at more than a billion dollars, typically in the technology sector.
+        - Human Resources: Score calculated looking at the researchers in R&D, university-industry collaboration, skillset of university graduates, extent of staff training, and the availability of scientists & engineers.
+        - Tech Hubs: Number of tech hubs in each European region. A high number of tech hubs in a region is synonymous with it being a productive and proliferating location, given the abundance of opportunities and advantages for tech entities.
+        - Government: Score calculated looking at how the government ensures policy stability, the legal frameworks' adaptability to digital business models, the efficiency in settling disputes, the efficiency in challenging regulations, the burden of governmental regulation, the number of days to start a business and e-Gov services.
+        - Infrastructure: Score calculated by observing at each region's 4G coverage, fiber coverage, Internet bandwith per user, 5G commercial networks, number of Internet exchange points, the number and maturity of 5G pilots, the time to get electricity, 4G's launch year and 5G spectrum auction plans.
+        """
+    )
+    
+    st.subheader("How to read the scores obtained:")
+    st.write("Standard scores are given in a -1 to 1 scale.")
+    st.write(
+        """
+        - A score of 0 represents the mean score of the regions.
+        - A score of -1 represents the region is in the lower 15.9% of the regions. Lower scores correspond to left outliers.
         - A score of 1 represents the region is in the top 15.9% of the regions. Higher scores correspond to right outliers.
         """
     )
