638145c9471956d6fe101a84783d3190d704f52d
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-08-19 00:50:17 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -245,7 +245,7 @@ def recommendation():
            ),
        ],
     ))
-    
+    st.write("To make a new search, just go back to the top and modify your configuration!")
 
 #def page2():
     #projects = pd.read_excel('ICT_H2020.xlsx', 'Proyectos')
