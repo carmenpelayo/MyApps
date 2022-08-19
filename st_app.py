a6896d69c9b80f340c9136496e37e2328c8ef107
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-08-19 14:03:44 +0200

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -14,9 +14,13 @@ from scipy.stats import zscore
 def home():
     st.title("Welcome to my world!")
     st.subheader("This is Carmen, a recently-graduated business strategist and program coder looking for exciting new challenges to engage in.")
-    st.write('What brings you here?')
+    st.write('**What brings you here?**')
     mot_vals = ['Just curiosity.', 'Your profile seems interesting.', 'I want you in my team!']
     motivation = st.radio("", tuple(mot_vals), key=3)
+    if motivation == "I want you in my team!":
+        st.write("Happy to hear that! You can contact me at:")
+        link = 'https://www.linkedin.com/in/carmenpelayofernandez/'
+        st.markdown(link, unsafe_allow_html=True)
 
 def main_page():
     st.title("Location Recommendation System for Businesses in the European ICT Industry")
