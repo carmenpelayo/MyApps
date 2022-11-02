0e44a4f34251ece5fc132cd797dbdbbe2d124d9b
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2022-11-02 02:08:25 +0100

    Update st_app.py

--- Personal-Web/st_app.py
+++ Personal-Web/st_app.py
@@ -72,6 +72,8 @@ if motivation == "I want you in my team!":
     st.write("😁 Happy to hear that! You can contact me at:")
     linkedin = 'https://www.linkedin.com/in/carmenpelayofernandez/'
     st.markdown(linkedin, unsafe_allow_html=True)
+    st.write("[LinkedIn](%s)" % linkedin)
+    st.markdown("[LinkedIn](%s)" % linkedin)
     st.write("carmenpelayofdez@gmail.com")
     counts_hire +=1
 elif motivation == "Just curiosity.":
@@ -79,32 +81,3 @@ elif motivation == "Just curiosity.":
     counts_cur += 1
    
    
-   #------------
-from streamlit.components.v1 import html
-
-def nav_page(page_name, timeout_secs=100):
-    nav_script = """
-        <script type="text/javascript">
-            function attempt_nav_page(page_name, start_time, timeout_secs) {
-                var links = window.parent.document.getElementsByTagName("a");
-                for (var i = 0; i < links.length; i++) {
-                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
-                        links[i].click();
-                        return;
-                    }
-                }
-                var elasped = new Date() - start_time;
-                if (elasped < timeout_secs * 1000) {
-                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
-                } else {
-                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
-                }
-            }
-            window.addEventListener("load", function() {
-                attempt_nav_page("%s", new Date(), %d);
-            });
-        </script>
-    """ % (page_name, timeout_secs)
-    html(nav_script)
-if st.button("Location Recommender"):
-    nav_page("https://locationrecommender.streamlitapp.com/")
